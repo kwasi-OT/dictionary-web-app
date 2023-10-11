29c39efc91fff9d72f4c5b19a1c01dfb0e99bcd6
Kwasi O-T <kwasi@kudigo.com>
2023-10-11 11:41:23 +0000

    added size and border-radius to form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = () => {
     const {darkTheme} = useContext(ThemeContext);
 
     return (
-        <div className={`d-flex justify-content-between align-items-center w-90 px-3 py-2 form-wrapper ${darkTheme ? 'dark' : ''}`}>
+        <div className={`d-flex justify-content-between align-items-center w-90 px-3 py-2 formWrapper ${darkTheme ? 'dark' : ''}`}>
             <div className='d-flex justify-content-between align-items-center  w-100'>
                 <input type='text' placeholder='Search GitHub username…' className={`form-input form-control border border-0 ${darkTheme ? 'dark' : ''}`}/>
                 <CiSearch color='#0079FF' size='1.5rem'/>
