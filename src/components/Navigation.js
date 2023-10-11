a1232fdc43eeb88d94a6fbeae4997f93e1806502
Kwasi O-T <kwasi@kudigo.com>
2023-10-11 05:44:32 +0000

    imported and inserted icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {RiBook2Line} from 'react-icons/ri';
+import {HiOutlineMoon} from 'react-icons/hi2';
+import {LiaCircle} from 'react-icons/lia';
+import {IoIosArrowDown} from 'react-icons/io';
 
 const Navigation = () => {
     return (
-        <div>
-        
+        <div className='d-flex'>
+            <div>
+                <RiBook2Line/>
+            </div>
+            <div>
+                <div>
+                    <p>Serif</p>
+                    <IoIosArrowDown/>
+                </div>
+                <div>
+                    <span>
+                        <LiaCircle/>
+                    </span>
+                    <HiOutlineMoon/>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Navigation
+export default Navigation;
