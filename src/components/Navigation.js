062f6867a79dc6288cb3c8915f7e1fe6727d3e02
Kwasi O-T <kwasi@kudigo.com>
2023-10-14 16:57:27 +0000

    adjusted styling for fontstyle selector

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {RiBook2Line} from 'react-icons/ri';
 import {HiOutlineMoon} from 'react-icons/hi2';
 import {LiaCircle} from 'react-icons/lia';
-import {IoIosArrowDown} from 'react-icons/io';
+// import {IoIosArrowDown} from 'react-icons/io';
 
 const Navigation = () => {
     return (
@@ -10,13 +10,16 @@ const Navigation = () => {
             <div className='iconContainer'>
                 <RiBook2Line color='#757575' className='bookIcon'/>
             </div>
-            <div className='rightNav d-flex justify-content-between align-items-center'>
-                <div className='fontSelect d-flex justify-content-between align-items-center'>
-                    <p>Serif</p>
-                    <IoIosArrowDown/>
+            <div className='rightNav d-flex justify-content-end align-items-center'>
+                <div className='fontSelect'>
+                    <select className='border border-0'>
+                        <option>Sans Serif</option>
+                        <option>Serif</option>
+                        <option>Mono</option>
+                    </select>
                 </div>
-                <div className='toggleContainer'>
-                    <span className='toggleButton'>
+                <div className='toggleContainer d-flex justify-content-center align-items-center'>
+                    <span className='toggleButton w-10 h-3'>
                         <LiaCircle/>
                     </span>
                     <HiOutlineMoon/>
