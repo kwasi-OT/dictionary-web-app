6f5c8ac7c69d0f93d83242db8ab662bbb33eeb5a
Kwasi O-T <kwasi@kudigo.com>
2023-10-15 01:48:29 +0000

    added styling and dark theme to body section

--- src/components/Display.js
+++ src/components/Display.js
@@ -21,40 +21,41 @@ const Display = () => {
             </div>
             <div className={`d-flex flex-row `}>
                 <span>
-                    <h4>noun</h4>
+                    <h4 className={`subTitle ${darkTheme ? 'dark' : ''}`}>noun</h4>
                 </span>
                 <span>
                     <hr color='#fff' />
                 </span>
             </div>
             <div>
-                <p>Meaning</p>
-                <ul>
-                    <li>(etc.) A set of keys used to operate a typewriter, computer etc.</li>
-                    <li>A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.</li>
-                    <li>A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.</li>
+                <p className='source'>Meaning</p>
+                <ul className={`list ${darkTheme ? 'dark' : ''}`}>
+                    <li><span className={`listList ${darkTheme ? 'dark' : ''}`}>(etc.) A set of keys used to operate a typewriter, computer etc.</span></li>
+                    <li><span className={`listList ${darkTheme ? 'dark' : ''}`}>A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.</span></li>
+                    <li><span className={`listList ${darkTheme ? 'dark' : ''}`}>A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.</span></li>
                 </ul>
             </div>
-            <div className={`d-flex`}>
-                <p>Synonyms</p>
-                <p>electronic keyboard</p>
+            <div className={`d-flex gap-4 align-items-center`}>
+                <p className='source'>Synonyms</p>
+                <p className='syno'>electronic keyboard</p>
             </div>
             <div>
-                <h4>verb</h4>
+                <h4 className={`subTitle ${darkTheme ? 'dark' : ''}`}>verb</h4>
             </div>
             <div>
                 <div>
-                    <p>Meaning</p>
+                    <p className='source'>Meaning</p>
                 </div>
                 <div>
-                    <ul>
-                        <li>To type on a computer keyboard.</li>
+                    <ul className={`list ${darkTheme ? 'dark' : ''}`}>
+                        <li><span className={`listList ${darkTheme ? 'dark' : ''}`}>To type on a computer keyboard.</span></li>
                     </ul>
-                    <p>“Keyboarding is the part of this job I hate the most.”</p>
+                    <p className={`verbKeyboarding ${darkTheme ? 'dark' : ''}`}>“Keyboarding is the part of this job I hate the most.”</p>
                 </div>
             </div>
-            <div className={`source d-flex gap-4 align-items-center ${darkTheme ? 'dark' : ''}`}>
-                <p>Source</p>
+            <hr color='#757575'/>
+            <div className={`d-flex gap-4 align-items-center ${darkTheme ? 'dark' : ''}`}>
+                <p className='source'>Source</p>
                 <div className={`bottomLink d-flex gap-2 align-items-center ${darkTheme ? 'dark' : ''}`}>
                     <p>https://en.wiktionary.org/wiki/keyboard</p>
                     <FaArrowUpRightFromSquare size='0.75rem' className='mb-3'/>
